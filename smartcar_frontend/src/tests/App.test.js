ed9a56bffa060c46c6322f84d532718475b0089c
Drake Axelrod <gusaxedr@student.gu.se>
2021-04-30 15:17:19 +0200

    Fix Action and add couple test cases
    Added some simple tests for javaascript, and fixed the github action.

--- smartcar_frontend/src/tests/App.test.js
+++ smartcar_frontend/src/tests/App.test.js
@@ -1,8 +1,14 @@
 import { render, screen } from '@testing-library/react';
 import App from '../App';
 
-test('renders learn react link', () => {
+test('has homepage', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const smartcar = screen.getAllByText(/SmartCar Shield/i);
+  const race = screen.getAllByText(/Race/i);
+  const leaderboard = screen.getByText(/Leaderboard/i);
+  const race_times = screen.getByText(/Race Times/i);
+  expect(smartcar);
+  expect(race);
+  expect(leaderboard);
+  expect(race_times);
 });
