3326162a576e1449b2aed79a9255e03bd37e77fa
VernitaJ <vernitajones87@gmail.com>
2021-05-11 14:18:34 +0200

    Minor commits in contribution to the mapping com

--- smartcar_frontend/src/components/camera/Camera.js
+++ smartcar_frontend/src/components/camera/Camera.js
@@ -9,18 +9,17 @@ const Camera = () => {
   const WIDTH = 320;
   const HEIGHT = 240;
   client.on("message", (topic, message) => {
-    console.console.log(message);
     const ctx = document.getElementById("canvas").getContext("2d");
-    const canvas = document.getElementById("canvas")
+    const canvas = document.getElementById("canvas");
     const arrayBuffer = new ArrayBuffer(WIDTH * HEIGHT * 4);
     const pixels = new Uint8ClampedArray(arrayBuffer);
     for (let y = 0; y < HEIGHT; y += 4) {
       for (let x = 0; x < WIDTH; x += 4) {
         const i = y * WIDTH + x;
-        pixels[i] = message[3 * i];         // red
+        pixels[i] = message[3 * i]; // red
         pixels[i + 1] = message[3 * i + 1]; // green
         pixels[i + 2] = message[3 * i + 2]; // blue
-        pixels[i + 3] = 255;                // alpha
+        pixels[i + 3] = 255; // alpha
       }
     }
     ctx.webkitImageSmoothingEnabled = false;
@@ -28,10 +27,20 @@ const Camera = () => {
     ctx.imageSmoothingEnabled = false;
     const imageData = new ImageData(pixels, WIDTH, HEIGHT);
     ctx.putImageData(imageData, 0, 0);
-    ctx.globalCompositeOperation = 'copy';
-    ctx.drawImage(canvas, 0,0, imageData.width, imageData.height, 0,0, WIDTH*6, HEIGHT*6);
+    ctx.globalCompositeOperation = "copy";
+    ctx.drawImage(
+      canvas,
+      0,
+      0,
+      imageData.width,
+      imageData.height,
+      0,
+      0,
+      WIDTH * 6,
+      HEIGHT * 6
+    );
   });
-  return <canvas id="canvas" height={HEIGHT * 6} width={WIDTH*6 }/>;
+  return <canvas id="canvas" height={HEIGHT * 6} width={WIDTH * 6} />;
 };
 
 export default Camera;
